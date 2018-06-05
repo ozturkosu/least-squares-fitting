2f2f180a7e552eca1312b432da8a9ea8c0c84c6b
yanastaneva8 <yanastaneva8@gmail.com>
2018-06-05 16:16:41 +0200

    Modified Gram-Schmidt

---
+++ modifiedGramSchmidt.m
@@ -0,0 +1,43 @@
+% 2019862s 
+% Assessed Coursework 1: Least Squares Fitting
+
+function [Q,R]=modifiedGramSchmidt(A)
+
+% Store dimensions of matrix A
+[m,n]=size(A);
+
+% Inititate zero matrices for the reduced Q and R
+Q=zeros(m,n);
+R=zeros(m,n);
+
+% Initiate a zero column vector 
+v=zeros(m,1);
+
+% For loop for extracting columns from the matrix A
+for i=1:n;
+    v(:,i)=A(:,i);
+end
+
+% For loop which implements the modified Gram-Schmidt
+for i=1:n;
+    % Diagonal entry of R is the 2-norm of the i-th column vector 
+    R(i,i)=norm(v(:,i),2);
+    % Compute the i-th column of Q 
+    Q(:,i)=v(:,i)/R(i,i);
+       for j=i+1:n;
+           % Compute the off-diagonal entries of R
+           R(i,j)=Q(:,i)'*v(:,j);
+           % Update the next column vector to be used 
+           % in the next iteration
+           v(:,j)=v(:,j)-R(i,j)*Q(:,i);
+       end
+end
+
+% Make sure to extract the reduced QR
+R=R(1:n,:);
+Q=Q(:,1:n);
+
+
+        
+    
+    
